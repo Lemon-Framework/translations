cfb446ccff163edc2359f9ebb2b6d011af8bcdbc
FungY911 <74965749+FungY911@users.noreply.github.com>
2023-04-15 17:50:45 +0200

    Create validation.php

---
+++ translations/sk/validation.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'numeric' => 'Hodnota %field musí byť číselná.',
+    'notNumeric' => 'Hodnota %field nesmie byť číselná.',
+    'email' => 'Hodnota %field musí byť platná e-mailová adresa.',
+    'url' => 'Hodnota %field musí byť platná URL adresa.',
+    'color' => 'Hodnota %field musí byť platná hexadecimálna farba.',
+    'max' => 'Hodnota %field musí byť menšia alebo rovnaká ako %arg.',
+    'min' => 'Hodnota %field musí byť väčšia alebo rovnaká ako %arg.',
+    'regex' => 'Hodnota %field nie je platná.',
+    'notRegex' => 'Hodnota %field nie je platná.',
+    'contains' => 'Hodnota %field nie je platná.',
+    'doesntContain' => 'Hodnota %field nie je platná.',
+    'missing' => 'Hodnota %field chýba.',
+];
